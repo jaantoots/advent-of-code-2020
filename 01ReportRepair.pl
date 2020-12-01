814007efee638dcc2c8c02210a56f9d664d00047
Jaan Toots <jaan@jaantoots.org>
2020-12-01 14:54:56 +0200

    Add Day 1 Part Two

--- 01ReportRepair.pl
+++ 01ReportRepair.pl
@@ -5,12 +5,26 @@ use warnings;
 use feature 'say';
 
 my $n = 2020;
-my @array = (0) x ($n + 1);
+my @entries = (0) x ($n + 1);
+my @sums = (0) x ($n + 1);
+
+my $two = 0;
+my $three = 0;
 
 while (<>) {
-    if ($array[2020 - $_]) {
-        say $_ * (2020 - $_);
-        last;
+    if ($entries[2020 - $_]) {
+        $two = $_ * (2020 - $_);
     }
-    $array[$_] = 1;
+    if ($sums[2020 - $_]) {
+        $three = $_ * $sums[2020 - $_];
+    }
+    for (my $i = 0; $i <= 2020 - $_; $i++) {
+        if ($entries[$i]) {
+            $sums[$_ + $i] = $_ * $i;
+        }
+    }
+    $entries[$_] = 1;
 }
+
+say $two;
+say $three;
