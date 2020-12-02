6ac3b7efe471d61f874f6dccf45076e4fde86402
Jaan Toots <jaan@jaantoots.org>
2020-12-02 16:08:36 +0200

    Add Day 2 Part Two

--- 02PasswordPhilosophy.pl
+++ 02PasswordPhilosophy.pl
@@ -5,13 +5,18 @@ use warnings;
 use feature 'say';
 
 my $valid = 0;
+my $valid_two = 0;
 
 while (<>) {
     $_ =~ /(\d+)-(\d+) (\w): (\w+)/;
     my $min = $1;
     my $max = $2;
-    my $count = () = $4 =~ /\Q$3/g;
+    my $char = $3;
+    my $pwd = $4;
+    my $count = () = $pwd =~ /\Q$char/g;
     $valid++ if ($min <= $count and $count <= $max);
+    $valid_two++ if (substr($pwd, $min - 1, 1) eq $char xor substr($pwd, $max - 1, 1) eq $char);
 }
 
 say $valid;
+say $valid_two;
