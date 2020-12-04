ff13391b266481f3052490203dc1903c9d4f67c9
Jaan Toots <jaan@jaantoots.org>
2020-12-04 13:52:16 +0200

    Add Day 3 Part Two

--- 03TobogganTrajectory.pl
+++ 03TobogganTrajectory.pl
@@ -4,19 +4,25 @@ use strict;
 use warnings;
 use feature 'say';
 
-my $trees = 0;
-my $right = 3;
-my $down = 1;
+my @trees = (0) x 5;
+my @right = (1, 3, 5, 7, 1);
+my @down = (1, 1, 1, 1, 2);
 
 my $row = 0;
-my $col = 0;
+my @col = (0) x 5;
 while (<>) {
     chomp $_;
-    if ($row % $down == 0) {
-        $trees++ if (substr($_, $col % length, 1) eq '#');
-        $col += $right;
+    for (my $i = 0; $i < 5; $i++) {
+        if ($row % $down[$i] == 0) {
+            $trees[$i]++ if (substr($_, $col[$i] % length, 1) eq '#');
+            $col[$i] += $right[$i];
+        }
+        $row++;
     }
-    $row++;
 }
 
-say $trees;
+my $prod = 1;
+$prod *= $_ foreach (@trees);
+
+say $trees[1];
+say $prod;
