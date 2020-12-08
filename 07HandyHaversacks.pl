ba3048ddc5f148ab0d0d1167a6881b94b9b06c31
Jaan Toots <jaan@jaantoots.org>
2020-12-08 20:28:32 +0200

    Add Day 7 Part Two

--- 07HandyHaversacks.pl
+++ 07HandyHaversacks.pl
@@ -5,13 +5,16 @@ use warnings;
 use feature 'say';
 
 my %contains;
+my %contained_by;
 while (<>) {
     chomp;
     /^(\w+ \w+)/;
     my $outer = $1;
-    while (/\d+ (\w+ \w+)/g) {
-        $contains{$1} = [] unless defined $contains{$1};
-        push(@{$contains{$1}}, ($outer));
+    $contained_by{$outer} = {};
+    while (/(\d+) (\w+ \w+)/g) {
+        $contains{$2} = [] unless defined $contains{$2};
+        push(@{$contains{$2}}, ($outer));
+        $contained_by{$outer}{$2} = $1;
     }
 }
 
@@ -27,4 +30,14 @@ sub find {
 }
 find("shiny gold");
 
+sub find_by {
+    my $bag = shift;
+    my $num = 0;
+    foreach my $inner (keys %{$contained_by{$bag}}) {
+        $num += $contained_by{$bag}{$inner} * (1 + find_by($inner));
+    }
+    return $num;
+}
+
 say scalar keys %contain_sg;
+say find_by("shiny gold");
