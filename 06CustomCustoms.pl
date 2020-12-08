b8c6533d9417f72e8c47d74ae64f33a5951c5f99
Jaan Toots <jaan@jaantoots.org>
2020-12-08 02:03:40 +0200

    Add Day 6 Part Two

--- 06CustomCustoms.pl
+++ 06CustomCustoms.pl
@@ -5,16 +5,24 @@ use warnings;
 use feature 'say';
 
 my $sum = 0;
+my $suma = 0;
 my %chars;
+my $all;
 while (<>) {
     chomp;
     if (/^$/) {
         $sum += scalar keys %chars;
         %chars = ();
+        $suma += length $all;
+        $all = undef;
         next;
     }
+    $all = $_ unless (defined $all);
+    $all =~ s/[^\Q$_\E]//g;
     $chars{$1} = () while (/(.)/g);
 }
 $sum += scalar keys %chars;
+$suma += length $all;
 
 say $sum;
+say $suma;
