c3fcfb9646491e00dfb280d80b7003ebbac6cd52
Jaan Toots <jaan@jaantoots.org>
2020-12-09 01:03:52 +0200

    Add Day 8 Part Two

--- 08HandheldHalting.pl
+++ 08HandheldHalting.pl
@@ -10,19 +10,35 @@ while (<>) {
     push(@instructions, ($_));
 }
 
-my @visited = (0) x (scalar @instructions);
-my $pos = 0;
-my $acc = 0;
-while (1) {
-    last if $visited[$pos];
-    $visited[$pos] = 1;
-    my ($op, $arg) = split(/ /, $instructions[$pos]);
-    if ($op eq "jmp") {
-        $pos += $arg;
-        next;
+sub run {
+    my $change = shift;
+    my $can_change = 0;
+    my @visited = (0) x (scalar @instructions);
+    my $pos = 0;
+    my $acc = 0;
+    while (1) {
+        last if ($pos > $#instructions or $visited[$pos]);
+        $visited[$pos] = 1;
+        my ($op, $arg) = split(/ /, $instructions[$pos]);
+        $op =~ tr/nojm/jmno/ if ($can_change == $change);
+        $can_change++ if ($op eq "jmp" or $op eq "nop");
+        if ($op eq "jmp") {
+            $pos += $arg;
+            next;
+        }
+        $acc += $arg if ($op eq "acc");
+        $pos++;
     }
-    $acc += $arg if ($op eq "acc");
-    $pos++;
+    return ($pos, $acc);
 }
 
-say $acc;
+say scalar run(-1);
+
+my $right;
+for (0..$#instructions) {
+    my ($pos, $acc) = run($_);
+    if ($pos > $#instructions) {
+        $right = $acc;
+        say $right;
+    }
+}
