81269426499971f678c1f7f9e7e6e5c90f62a7f0
Jaan Toots <jaan@jaantoots.org>
2020-12-09 17:37:41 +0200

    Add Day 9 Part Two

--- 09EncodingError.pl
+++ 09EncodingError.pl
@@ -6,9 +6,12 @@ use feature 'say';
 
 my @numbers;
 my @sums;
+my $invalid;
+my @all_numbers;
 while (<>) {
     chomp;
     push(@numbers, ($_));
+    push(@all_numbers, ($_));
     for (my $i = 0; $i < $#numbers; $i++) {
         push(@{$sums[$i]}, ($numbers[$i] + $_));
     }
@@ -22,10 +25,29 @@ while (<>) {
             }
         }
     }
-    unless ($found) {
-        say $_;
-        last;
+    unless ($found or defined $invalid) {
+        $invalid = $_;
     }
     shift @numbers;
     shift @sums;
 }
+
+say $invalid;
+
+my $i;
+my $j;
+LOOP: for ($i = 0; $i <= $#all_numbers; $i++) {
+    my $sum = 0;
+    for ($j = $i; $j <= $#all_numbers; $j++) {
+        $sum += $all_numbers[$j];
+        last LOOP if ($sum == $invalid);
+    }
+}
+my $min = $all_numbers[$i];
+my $max = $all_numbers[$i];
+for (@all_numbers[$i..$j]) {
+    $min = $_ if ($_ < $min);
+    $max = $_ if ($_ > $max);
+}
+
+say $min + $max;
